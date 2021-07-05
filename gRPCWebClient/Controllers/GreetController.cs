537933b7d751f89752b9ec4f7360a834c389befd
Mihaela Radulescu <mihaela.radulescu@mercell.com>
2021-07-05 16:00:32 +0300

    application calls method from another application, through gRPC

---
+++ gRPCWebClient/Controllers/GreetController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace gRPCWebClient.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GreetController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<string> Greet()
+        {
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            
+            //call to gRPCWebServer through gRPC
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Mihaela" });
+
+            return reply.Message;
+        }
+    }
+}
---
+++ gRPCWebServer/Services/GreeterService.cs
@@ -0,0 +1,16 @@
+using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace gRPCWebServer
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = $"Hello, {request.Name}!"
+            });
+        }
+    }
+}
